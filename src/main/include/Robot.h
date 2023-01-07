96c2535395781c7775996adf939384f0f9f22e03
ThatAlo <52977682+ThatAlo@users.noreply.github.com>
2023-01-07 13:33:26 -0700

    Version 2 of swerve drive code
    Enhanced swerve drive code.
    
    Now with:
        > Optimized angle finding
        > Auto wheel orientation
        > Commented code!

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -43,26 +43,30 @@ class Robot : public frc::TimedRobot {
   void TestInit() override;
   void TestPeriodic() override;
 
-
+  //**************************************************************Drive Loop******************************************************
   void drive(double FWD, double STR, double RCW){
     yaw = ahrs->GetYaw();
     double rrsa = m_rrsensor.GetPosition();
-    //yaw = ahrs->GetAngle();
     if(yaw<0){
       yaw *= -1;
       yaw = (360 - yaw); 
     }
-    //std::cout<<yaw<<std::endl;
+
     yaw = (yaw * (M_PI/180));
 
     double temp = ((FWD * cos(yaw)) + STR * sin(yaw));
     STR = ((-FWD) * sin(yaw)) + (STR * cos(yaw));
     FWD = temp;
-    //RCW *= 10;
-    //std::cout<<"FWD: "<<FWD<<" STR: "<< STR <<" ANGLE: "<<yaw<<std::endl;
 
-    L = 25.75; //wheelbase (from center of front wheel to center of back wheel) unit doesn't matter because these will only be used as ratios but works well with meters
-	  W = 25.75; //wheelbase (from center of left wheel to center of right wheel) unit doesn't matter because these will only be used as ratios but works well with meters
+
+
+
+    //********Setting ratios for wheel distance********
+    //wheelbase (from center of front wheel to center of back wheel) unit doesn't matter because these will only be used as ratios but works well with meters
+    //IMPORTANT!!!    THESE TWO NUMBERS ARE WRONG!!!    >>> MUST CHANGE "L" & "W" WITH NEW FRAME <<<
+    L = 25.75;
+	  W = 25.75;
+
 	  R = sqrt((L * L) + (W * W));
 
 	  A = STR - RCW * (L / R);
@@ -70,60 +74,49 @@ class Robot : public frc::TimedRobot {
 	  C = FWD - RCW * (W / R);
 	  D = FWD + RCW * (W / R);
 
+
+
+
+    //********Set wheel angles and speeds********
 	  ws1 = sqrt((B * B) + (C * C));
 	  wa1 = atan2(B, C) * 180 / M_PI;
+    wa1 *= conversionFactor;
+
 	  ws2 = sqrt((B * B) + (D * D));
 	  wa2 = atan2(B, D) * 180 / M_PI;
+    wa2 *= conversionFactor;
+
 	  ws3 = sqrt((A * A) + (D * D));
 	  wa3 = atan2(A, D) * 180 / M_PI;
+    wa3 *= conversionFactor;
+
 	  ws4 = sqrt((A * A) + (C * C));
 	  wa4 = atan2(A, C) * 180 / M_PI;
+    wa4 *= conversionFactor;
 
-    /*if ((abs(wa1 - oldwa1) > 90) && (abs(wa1 - oldwa1) < 270)) {
-      if(oldwa1<0){
-        oldwa1 *= -1;
-        oldwa1 = (360 - oldwa1);
-      }
-      wa1 = (wa1 + 180.0);
-      wa1 = fmod(wa1, 360);
-      ws1 *= -1;
-    }
 
-    if ((abs(wa2 - oldwa2) > 90) && (abs(wa2 - oldwa2) < 270)) {
-      if(oldwa2<0){
-        oldwa2 *= -1;
-        oldwa2 = (360 - oldwa2);
-      }
-      wa2 = (wa2 + 180.0);
-      wa2 = fmod(wa2, 360);
-      ws2 *= -1;
-    }
 
-    if ((abs(wa3 - oldwa3) > 90) && (abs(wa3 - oldwa3) < 270)) {
-      if(oldwa3<0){
-        oldwa3 *= -1;
-        oldwa3 = (360 - oldwa3);
-      }
-      wa3 = (wa3 + 180.0);
-      wa3 = fmod(wa3, 360);
-      ws3 *= -1;
-    }
 
-    if ((abs(wa4 - oldwa4) > 90) && (abs(wa4 - oldwa4) < 270)) {
-      if(oldwa4<0){
-        oldwa4 *= -1;
-        oldwa4 = (360 - oldwa4);
-      }
-      wa4 = (wa4 + 180.0);
-      wa4 = fmod(wa4, 360);
-      ws4 *= -1;
-    }*/
+    //*********Optimize wheel angles********
+    angleitize(oldwa1,wa1);
+    wa1=outputAngle;
+    ws1*=direction;
 
-	  wa1 *= conversion;
-	  wa2 *= conversion;
-	  wa3 *= conversion;
-	  wa4 *= conversion;
+    angleitize(oldwa2,wa2);
+    wa2=outputAngle;
+    ws2*=direction;
 
+    angleitize(oldwa3,wa3);
+    wa3=outputAngle;
+    ws3*=direction;
+
+    angleitize(oldwa4,wa4);
+    wa4=outputAngle;
+    ws4*=direction;
+
+
+
+    //********Set max speed********
 	  double max = ws1;
 	  if (ws2 > max)max = ws2;
 	  if (ws3 > max)max = ws3;
@@ -131,29 +124,98 @@ class Robot : public frc::TimedRobot {
 	  if (max > 1) { ws1 /= max; ws2 /= max; ws3 /= max; ws4 /= max; }
 
 
+
+
+    //********Set angle motor position********
     m_rra.Set(TalonFXControlMode::Position, wa4);
     m_fra.Set(TalonFXControlMode::Position, wa1);
     m_rla.Set(TalonFXControlMode::Position, wa3);
     m_fla.Set(TalonFXControlMode::Position, wa2);
 
-    std::cout<<"cancoder: "<<rrsa<<" angle: "<<wa4<<std::endl;
 
+
+
+    //********Set old angles********
     oldwa1 = wa1;
     oldwa2 = wa2;
     oldwa3 = wa3;
     oldwa4 = wa4;
 
-    double PercentOut = 0.2;
 
-    m_rrd.Set(PercentOut*ws4);
+
+
+    //********Drive Speed!********
+    double PercentOut = -0.3; //must be negative because I dont want to go through the effort of recalibrating the magnet offsets again
+
+
+
+
+    //********Set Drive Motors********
     m_frd.Set(PercentOut*ws1);
-    m_rld.Set(PercentOut*ws3);
     m_fld.Set(PercentOut*ws2);
+    m_rld.Set(PercentOut*ws3);
+    m_rrd.Set(PercentOut*ws4);
+  }                             
+
+  //***************************************************Angle Optimization Algorithm**************************************************
+  void angleitize(double current, double desired){
+	  direction = 1;
+	  angle1 = (desired - (fmod(current,4096)));
+  	angle2 = ((2048 - abs(fmod(desired,4096))) + (2048 - abs(fmod(current,4096))));
+
+  	if (abs(angle1) <= abs(angle2)) {
+  		shortest = angle1;
+  		check1 = false;
+  	}
+  	else {
+  		shortest = angle2;
+  		check1 = true;
+  	}
+
+	angle1 = ((desired - 2048) - fmod(current,4096));
+	angle2 = ((2048 - (abs(fmod(desired,4096))-2048)) + (2048 - (abs(fmod(current,4096)))));
+	
+	if (abs(angle2) < abs(angle1)) {
+		shortest2 = angle2;
+		check2 = true;
+	}
+	else {
+		shortest2 = angle1;
+		check2 = false;
+	}
+
+	if (abs(shortest) <= abs(shortest2)) {
+		if (check1 == true) {
+			if (current < 0) {
+				current -= shortest;
+			}
+			else {
+				current += shortest;
+			}
+		}
+		else {
+			current += shortest;
+		}
+	}
+	else {
+		direction = -1;
+		if (check2 == true) {
+			if (current < 0) {
+				current -= shortest2;
+			}
+			else {
+				current += shortest2;
+			}
+		}
+		else {
+			current += shortest2;
+		}
+	}
+  outputAngle = current;
   }
 
-
   private:
-    //****************************************************CONTROLLERS****************************************************
+    //********************************************************CONTROLLERS********************************************************
     frc::XboxController m_driverController{0};
 
     //****************************************************MOTORS AND ENCODERS****************************************************
@@ -176,7 +238,7 @@ class Robot : public frc::TimedRobot {
     WPI_CANCoder m_flsensor{3};
 
     //****************************************************SWERVE DRIVE VARIABLES****************************************************
-    double conversion = 4096.0/ 360.0;
+    double conversionFactor = 4096.0/ 360.0;
 
     double L;
     double W;
@@ -204,4 +266,16 @@ class Robot : public frc::TimedRobot {
     //****************************************************NAVX-MXP****************************************************
     AHRS *ahrs;
     double yaw;
+
+    //*********************************************Swerve Drive Optimization******************************************
+    double direction;
+    double angle1;
+  	double angle2;
+  	double shortest;
+  	bool check1;
+    double shortest2;
+	  bool check2;
+
+    double outputAngle;
+
   };
